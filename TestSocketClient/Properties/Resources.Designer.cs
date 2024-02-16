4a2bfadb7df64252ccbef5fb91c6aeb87277a684
AppleChurr <srsten1@gmail.com>
2024-02-16 10:01:17 +0900

    소켓 통신 관련 공유프로젝트 sCommunication으로 병합 소켓 통신 관련 프로젝트 이름 변경

--- TestSocketClient/Properties/Resources.Designer.cs
+++ TestSocketClient/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SocketServerTest.Properties {
+namespace TestSocketClient.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SocketServerTest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SocketServerTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TestSocketClient.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
